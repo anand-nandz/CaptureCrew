7df51d5c487101464a3175e910260d0922fbc161
anand-nandz <150254910+anand-nandz@users.noreply.github.com>
2024-10-26 19:25:57 +0530

    updated by adding new constants

--- Frontend/src/config/constants/constants.ts
+++ Frontend/src/config/constants/constants.ts
@@ -2,9 +2,11 @@ export const USER ={
     SIGNUP: '/signup',
     LOGIN : '/login',
     VERIFY :'/verify',
-
+    FORGOT_PWDMAIL: "/forgot-password/:token",
 
     HOME : '/',
+    PROFILE : '/profile',
+    VENDORLIST : '/vendorList'
 
 }
 
@@ -13,9 +15,22 @@ export const VENDOR ={
     VENDOR : '/vendor',
     SIGNUP: '/vendor/signup',
     LOGIN : '/vendor/login',
-    VERIFY :'/vendor/verify',
-
+    VERIFY :'/vendor/verify-email',
+    PROFILE : '/vendor/profile',
+    FORGOT_PWDMAIL: "/forgot-password/:token",
 
     DASHBOARD : '/vendor/dashboard',
     
 }
+
+
+export const ADMIN ={
+    LOGIN : '/',
+    DASHBOARD : '/dashboard',
+    USERS : '/users' ,
+    VENDORS : '/vendors'
+    
+}
+
+
+
