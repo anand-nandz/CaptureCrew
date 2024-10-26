4c6e502489591ab992da7e2991164c582f1f8135
anand-nandz <150254910+anand-nandz@users.noreply.github.com>
2024-10-26 19:29:33 +0530

    user side ui updated

---
+++ Frontend/src/pages/common/sampleText.tsx
@@ -0,0 +1,147 @@
+import {
+    Tabs,
+    TabsHeader,
+    TabsBody,
+    Tab,
+    TabPanel,
+} from "@material-tailwind/react";
+
+export function PhotoGalleryWithTabs() {
+    const data = [
+        {
+            label: "Wedding",
+            value: "wedding",
+            images: [
+                {
+                    imageLink: "https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1511285560929-80b456fea0bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1606800052052-a08af7148866?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1549417229-7686ac5595fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1583939003579-730e3918a45a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1460978812857-470ed1c77af0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+            ],
+        },
+        {
+            label: "Engagement",
+            value: "engagement",
+            images: [
+                {
+                    imageLink: "https://images.unsplash.com/photo-1583939411023-14783179e581?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1591604466107-ec97de577aff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1589571894960-20bbe2828d0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1602673221577-0b56d7ce446b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1550005809-91ad75fb315f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+                },
+            ],
+        },
+        {
+            label: "Birthday Party",
+            value: "birthday",
+            images: [
+                {
+                    imageLink: "https://images.unsplash.com/photo-1530103862676-de8c9debad1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1558008258-3256797b43f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2091&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2036&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1530103862676-de8c9debad1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2036&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1533294455009-a77b7557d2d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+            ],
+        },
+        {
+            label: "Outdoor Shoot",
+            value: "outdoor",
+            images: [
+                {
+                    imageLink: "https://images.unsplash.com/photo-1623091410901-00e2d268901f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1617019114583-affb34d1b3cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1624555130581-1d9cca783bc0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1617019114583-affb34d1b3cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+                },
+                {
+                    imageLink: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+                },
+            ],
+        },
+    ];
+
+    return (
+        <div className="flex flex-col items-center">
+            <h2 className="text-4xl font-light tracking-[0.3em] text-[#B8860B] py-12 uppercase">
+                Popular Moments
+            </h2>
+
+            <div className="my-8 container  w-full"> {/* Added margin top and bottom */}
+                <Tabs value="wedding">
+                    <TabsHeader placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                        {data.map(({ label, value }) => (
+                            <Tab key={value} value={value} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                {label}
+                            </Tab>
+                        ))}
+                    </TabsHeader>
+                    <TabsBody className="grid grid-cols-1 gap-4 mt-4" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>  {/* Added margin top */}
+                        {data.map(({ value, images }) => (
+                            <TabPanel
+                                className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3"
+                                key={value}
+                                value={value}
+                            >
+                                {images?.map(({ imageLink }, index) => (
+                                    <div key={index} className="overflow-hidden rounded-lg">
+                                        <img
+                                            className="h-64 w-full object-cover object-center transition-transform duration-300 hover:scale-110"
+                                            src={imageLink}
+                                            alt={`${value}-photo-${index + 1}`}
+                                        />
+                                    </div>
+                                ))}
+                            </TabPanel>
+                        ))}
+                    </TabsBody>
+                </Tabs>
+            </div>
+        </div>
+    );
+}
---
+++ Frontend/src/pages/user/home/Descp.tsx
@@ -0,0 +1,43 @@
+import {
+    
+    Typography,
+    
+  } from "@material-tailwind/react";
+const Descp =()=>{
+    return(
+        <>
+          <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32 m-0">
+        <div className="absolute top-0 h-full w-full bg-[url('/images/vendorHome.jpg')] bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
+        <div className="max-w-8xl container relative mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-6 font-black text-4xl lg:text-5xl" 
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+              Empower Your Events, Elevate Your Business
+              </Typography>
+              <Typography
+                variant="lead"
+                color="white"
+                className="opacity-80 text-sm lg:text-2xl"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                Each event is an opportunity for you to shine. Let’s build something extraordinary together, turning every occasion into a masterpiece.
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+        </>
+    )
+}
+
+export default Descp
