fcc491b170fbe7a0ad4b7d34a987c817624db86b
anand-nandz <150254910+anand-nandz@users.noreply.github.com>
2024-11-18 14:25:10 +0530

    types updated

--- Backend/src/types/vendorTypes.ts
+++ Backend/src/types/vendorTypes.ts
@@ -4,5 +4,6 @@ import { Types } from 'mongoose';
 export interface VendorRequest extends Request {
     vendor?: {
         _id: Types.ObjectId;
+        // role?: 'user' | 'vendor' | 'admin';
     };
 }
