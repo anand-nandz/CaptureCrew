909c25f843b9979b647fdb0a79f04accf826895b
anand-nandz <150254910+anand-nandz@users.noreply.github.com>
2024-11-18 14:13:14 +0530

    dot env configured

--- Backend/src/services/s3Service.ts
+++ Backend/src/services/s3Service.ts
@@ -1,6 +1,8 @@
-import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from 'uuid';
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import dotenv from 'dotenv'
+dotenv.config()
 
 declare global {
     namespace NodeJS {
@@ -28,17 +30,19 @@ export class S3Service {
                 secretAccessKey: process.env.SECRET_ACCESS_KEY
             }
         });
+        
     }
 
     async getFile(folderPath:string , fileName: string | undefined) :  Promise<string> {
         try {
+            
             const getObjectparams = {
                 Bucket : this.bucketName,
                 Key :`${folderPath}${fileName}`
             }
-         
-            const getCommand = new GetObjectCommand(getObjectparams)
-            const url = await getSignedUrl(this.s3Client, getCommand, {expiresIn : 60 * 60})
+            
+            const getCommand = new GetObjectCommand(getObjectparams)           
+            const url = await getSignedUrl(this.s3Client, getCommand, {expiresIn : 60 * 60})            
             return url
 
         } catch (error) {
@@ -56,7 +60,6 @@ export class S3Service {
         const contentType : string = file.mimetype;
 
         const uniqueFileName = `${uuidv4()}-${fileName}`;
-        console.log(uniqueFileName, 'unique image name uploadtos3');
 
         try {
             await this.s3Client.send(new PutObjectCommand({
@@ -72,6 +75,21 @@ export class S3Service {
             throw new Error('Failed to upload file to S3');
         }
     }
+
+    async deleteFromS3(key: string): Promise<void> {
+        try {
+            await this.s3Client.send(new DeleteObjectCommand({
+                Bucket: this.bucketName,
+                Key: key
+            }));
+            
+        } catch (error) {
+            console.error('Error deleting from S3:', error);
+            throw new Error('Failed to delete file from S3');
+        }
+    }
+
+
 }
 
 export const s3Service = new S3Service();
