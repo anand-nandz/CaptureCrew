0045eb212ce4408932d1113948a10417aba3fa2d
anand-nandz <150254910+anand-nandz@users.noreply.github.com>
2024-11-18 14:28:59 +0530

    updated post fields

--- Backend/src/models/vendorModel.ts
+++ Backend/src/models/vendorModel.ts
@@ -1,57 +1,93 @@
-import mongoose,{Schema,Document,Model} from "mongoose";
+import mongoose, { Schema, Document, Model, Types } from "mongoose";
+
 export enum AcceptanceStatus {
     Requested = 'requested',
     Accepted = 'accepted',
     Rejected = 'rejected'
 }
+
 export interface Vendor {
-    email :string ;
-    password?:string ;
-    name: string ;
-    companyName:string;
-    city: string ;
-    about: string ;
-    contactinfo :string ;
-    isActive : boolean ;
-    isVerified : boolean ;
-    isAccepted:AcceptanceStatus;
-    logo : string ;
-    profilepic : string ;
-    totalBooking:number;
-    bookedDates:string[];
-    refreshToken:string;
-    totalRating:number;
+    email: string;
+    password?: string;
+    name: string;
+    companyName: string;
+    city: string;
+    about: string;
+    contactinfo: string;
+    isActive: boolean;
+    isVerified: boolean;
+    isAccepted: AcceptanceStatus;
+    logo: string;
+    imageUrl: string;
+    totalBooking: number;
+    bookedDates: string[];
+    postCount: number;
+    refreshToken: string;
+    totalRating: number;
+    posts?: Types.ObjectId[];
     resetPasswordToken?: string;
     resetPasswordExpires?: Date;
 }
+
 export interface VendorDocument extends Vendor, Document {
     _id: mongoose.Types.ObjectId;
-  }
+    email: string;
+    password?: string;
+    name: string;
+    companyName: string;
+    city: string;
+    about: string;
+    contactinfo: string;
+    isActive: boolean;
+    isVerified: boolean;
+    isAccepted: AcceptanceStatus;
+    logo: string;
+    imageUrl: string;
+    totalBooking: number;
+    bookedDates: string[];
+    postCount: number;
+    refreshToken: string;
+    totalRating: number;
+    posts?: Types.ObjectId[];
+    resetPasswordToken?: string;
+    resetPasswordExpires?: Date;
+}
 
-  export interface VendorModel extends Model<VendorDocument> {
+export interface VendorModel extends Model<VendorDocument> {
     // Add any static methods here if needed
-  }
-export interface VendorDocument extends Vendor ,Document {}
+}
 
 const VendorSchema = new Schema<VendorDocument>({
-    email : {type : String ,required: true , unique:true},
-    password : {type:String , required:true} ,
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     name: { type: String, required: true },
     companyName: { type: String, required: true },
     city: { type: String, required: true },
     about: { type: String },
-    contactinfo: { type: String ,required:true , unique:true},
+    contactinfo: { type: String, required: true, unique: true },
     isActive: { type: Boolean, default: true },
     isVerified: { type: Boolean, default: false },
     isAccepted: { type: String, enum: Object.values(AcceptanceStatus), default: AcceptanceStatus.Requested },
-    logo:{type:String, default:''},
-    profilepic : {type:String , default:''},
-    totalBooking:{type:Number},
-    bookedDates:{type:[String]},
-    totalRating:{type:Number,default:0},
+    logo: { type: String, default: '' },
+    imageUrl: { type: String, default: '' },
+    totalBooking: { type: Number },
+    bookedDates: { type: [String] },
+    postCount: {
+        type: Number,
+        default: 0
+    },
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Post"
+    }],
+    totalRating: { type: Number, default: 0 },
     refreshToken: { type: String },
-    resetPasswordToken: {type : String},
-    resetPasswordExpires: {type : Date},
-},{ timestamps: true })
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date },
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
 
-export default mongoose.model<VendorDocument>('Vendor',VendorSchema)
+export default mongoose.model<VendorDocument, VendorModel>('Vendor', VendorSchema);
